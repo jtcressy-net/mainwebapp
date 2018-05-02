feb6e502a2334d1dda0d299167415cd75b329029
jtcressy <joel@jtcressy.net>
2018-05-01 19:24:40 -0600

    Add nginx conf

--- mainwebapp/settings.py
+++ mainwebapp/settings.py
@@ -129,4 +129,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = '/var/www/static'
+STATIC_ROOT = '/app/static'
+MEDIA_ROOT = "/app/media"
+MEDIA_URL = '/media/'
